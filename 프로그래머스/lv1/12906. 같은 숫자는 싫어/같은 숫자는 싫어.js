444953864c191d4f9b3baff4843a435dc9d655e4
MinCheolS <97032929+MinCheolS@users.noreply.github.com>
2023-08-04 11:25:47 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 30.76 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,21 @@
+// function solution(arr) {
+//     let answer = [];
+    
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] !== arr[i+1]) {
+//             answer.push(arr[i])
+//         }
+//     }
+//     return answer;
+// }
+
+function solution(arr) {
+    let answer = [];
+    
+    for(let num of arr) {
+        if(num !== answer[answer.length - 1] ) {
+            answer.push(num)
+        }
+    }
+    return answer;
+}
