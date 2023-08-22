5011ee685aa4e55b39a3623ec67460838f0a3be7
MinCheolS <97032929+MinCheolS@users.noreply.github.com>
2023-08-22 09:35:41 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -10,11 +10,3 @@ function solution(citations) {
     }
     return answer;
 }
-
-// function solution(citations) {
-//     let i = 0;
-
-//     while(i + 1 <= citations.sort((a, b) => b - a)[i]) i++;
-
-//     return i;
-// }
