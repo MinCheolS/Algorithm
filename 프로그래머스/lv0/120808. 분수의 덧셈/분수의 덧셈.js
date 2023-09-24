700a0ca19fbb32f9f0aff2b8ad30ed6e84c174c5
MinCheolS <97032929+MinCheolS@users.noreply.github.com>
2023-09-24 20:14:06 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,10 @@
+function solution(numer1, denom1, numer2, denom2) {
+    let a = numer1 * denom2 + numer2 * denom1
+    let b = denom1 * denom2
+    let gcd = 1;
+
+    for(let i = 1; i <= a; i++) {
+        if(a % i === 0 && b % i === 0) gcd = i;
+    }
+    return [a / gcd , b / gcd]
+}
