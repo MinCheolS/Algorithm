3390faeb4a7cd232f7f760dd7bd1a123426f04d9
MinCheolS <97032929+MinCheolS@users.noreply.github.com>
2023-09-26 09:50:23 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = [];
+    
+    for(let i = 1; i <= n; i++) {
+        if(i % 2 === 1) {
+            answer.push(i)
+        }
+    }
+    
+    answer.sort((a, b) => a - b)
+    
+    return answer;
+}
