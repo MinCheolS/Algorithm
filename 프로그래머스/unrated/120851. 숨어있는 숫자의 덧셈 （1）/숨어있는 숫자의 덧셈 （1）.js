3407f2a78236590d9c0cb206aa73aa2ec722f467
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-10-22 13:45:01 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    let answer = [];
+    
+    for(let i = 0; i < my_string.length; i++) {
+        if(my_string[i] / 1 > 0) {
+            answer.push(Number(my_string[i]))
+        }
+    }
+    return answer.reduce((acc, cur) => acc + cur)
+}
