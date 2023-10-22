d370fb0b2b7b28a7c074ad956f63ab82c54c0a59
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-10-22 13:44:53 +0900

    [level unrated] Title: 문자열 정렬하기 (1), Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    let answer = [];
+    
+    for(let i = 0; i < my_string.length; i++) {
+        if(my_string[i] / 1 >= 0) {
+            answer.push(Number(my_string[i]))
+        }
+    }
+    answer.sort((a, b) => a - b)
+    
+    return answer;
+}
