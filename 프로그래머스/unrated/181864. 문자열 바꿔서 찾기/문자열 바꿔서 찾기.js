6280741baff4600df6c4ccc620c4f06b6c12babd
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-11-02 10:09:05 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -0,0 +1,10 @@
+function solution(myString, pat) {
+    let str = ''
+
+    for(let i = 0; i < myString.length; i++) {
+        if(myString[i] === 'A') str += 'B'
+        else str += 'A'
+    }
+    
+    return str.includes(pat) ? 1 : 0
+}
