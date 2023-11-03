d7f85a41b882b6a9960431fd4117146b15e3db1e
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-11-03 10:24:01 +0900

    [level unrated] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -1,11 +1,9 @@
 function solution(my_string) {
-    let answer = [];
-    let arr = my_string.split('')
+    let arr = [];
     
     for(let i = 0; i < my_string.length; i++) {
-        answer.push(arr.join(''))
-        arr.shift()
+        arr.push(my_string.slice(i))
     }
     
-    return answer.sort()
+    return arr.sort()
 }
