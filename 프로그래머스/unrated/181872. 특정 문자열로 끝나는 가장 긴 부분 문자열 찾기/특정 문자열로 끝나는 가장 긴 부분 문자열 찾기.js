bf0e4c8e9bb845947cb1f9e9f131c7e8bf775b5f
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-11-04 11:00:19 +0900

    [level unrated] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js
@@ -0,0 +1,13 @@
+function solution(myString, pat) {
+    let answer = ''
+    
+    for(let i = 0; i < myString.length; i++) {
+        let arr = myString.slice(i, pat.length + i)
+        
+        if(arr === pat) {
+            answer = myString.slice(0, pat.length + i)
+        }
+    }
+    
+    return answer
+}
