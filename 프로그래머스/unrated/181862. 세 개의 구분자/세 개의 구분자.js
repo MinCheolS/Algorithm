6bfd4609d7258d155a56ee6607ef001f914ea93e
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-11-04 11:31:09 +0900

    [level unrated] Title: 세 개의 구분자, Time: 15.55 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,10 +1,5 @@
 function solution(myStr) {
-//     let arr = myStr.split(/[a|b|c]/g).filter(a => a)
+    let arr = myStr.split(/[a|b|c]/g).filter(a => a)
 
-//     return arr.length ? arr : ['EMPTY']
-    const tmp1 = myStr.split("a").join("b")
-    const tmp2 = tmp1.split("b").join("c")
-    const tmp3 = tmp2.split("c").filter(x => x)
-    if (tmp3.length === 0) return ["EMPTY"]
-    return tmp3
+    return arr.length ? arr : ['EMPTY']
 }
