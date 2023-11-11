e7c5c50f974e40425a83072178c026386bccd215
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-11-11 11:59:25 +0900

    [level unrated] Title: 문자열 묶기, Time: 89.80 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
@@ -1,11 +1,11 @@
 function solution(strArr) {
-    let answer = {}
+    let answer = []
     let MaxNum = Math.max(...strArr.map((a) => a.length))
     
     for(let i = 1; i <= MaxNum; i++) {
         const arr = strArr.filter((a) => a.length === i)
-        answer[i] = arr.length
+        answer.push(arr.length)
     }
 
-    return Math.max(...Object.values(answer))
+    return Math.max(...answer)
 }
