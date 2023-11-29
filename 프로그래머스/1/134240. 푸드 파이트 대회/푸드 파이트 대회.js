bff3b33e9baa56de77ab379a3e3745a41c02bc85
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-11-29 11:20:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,13 +1,9 @@
 function solution(food) {
     let answer = ''
-    let reverse = ''
-    let count = 0
     
     for(let i = 1; i < food.length; i++) {
-        count = Math.floor(food[i] / 2)
-        answer += i.toString().repeat(count)
+        answer += i.toString().repeat(Math.floor(food[i] / 2))
     }
-    reverse = answer.split('').reverse().join('')
 
-    return [...answer, 0, ...reverse].join('')
+    return answer + '0' + [...answer].reverse().join('')
 }
