53b7be4cd3037b9b06000da9bd83e82f9b413bfb
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-12-07 15:31:37 +0900

    [level 1] Title: 옹알이 (2), Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,16 @@
+function solution(babbling) {
+    let arr = ["aya", "ye", "woo", "ma"]
+    let answer = 0
+    
+    for(let i = 0; i < babbling.length; i++) {
+        let babble = babbling[i]
+        for(let j = 0; j < arr.length; j++) {
+            if(babble.includes(arr[j].repeat(2))) break
+            
+            babble = babble.split(arr[j]).join(' ')
+        }
+        if(babble.split(' ').join('').length === 0) answer++
+    }
+    
+    return answer
+}
