c92b3e61d440bccab43134a8bd26978f8dfa28b1
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-12-15 11:32:24 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,14 @@
+function solution(wallpaper) {
+    let [top, bottom, left, right] = [51, 0, 51, 0];
+  
+    for (let i = 0; i < wallpaper.length; i++) {
+        for (let j = 0; j < wallpaper[0].length; j++) {
+            if (wallpaper[i][j] === ".") continue;
+            top = Math.min(top, i);
+            bottom = Math.max(bottom, i);
+            left = Math.min(left, j);
+            right = Math.max(right, j);
+        }
+    }
+  return [top, left, bottom + 1, right + 1];
+}
