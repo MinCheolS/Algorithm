d5c81153daa85de8b7d265b845b40a93db9dd8e2
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-12-24 16:02:38 +0900

    [Bronze V] Title: A+B - 8, Time: 132 ms, Memory: 9524 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+for (let i = 1; i <= +input[0]; i++) {
+  const A = +input[i].split(' ')[0]
+  const B = +input[i].split(' ')[1]
+
+  console.log(`Case #${i}: ${A} + ${B} = ${A + B}`)
+}
