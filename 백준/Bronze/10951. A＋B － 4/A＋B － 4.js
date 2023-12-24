853f59579c1065fc1dee9bb6c591a72176938b17
신민철 <97032929+MinCheolS@users.noreply.github.com>
2023-12-24 16:41:02 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 9564 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+for (let i = 0; i < input.length; i++) {
+  const num = input[i].trim().split(' ')
+  console.log(+num[0] + +num[1])
+}
