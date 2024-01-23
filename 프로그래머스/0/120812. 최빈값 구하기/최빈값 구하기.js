3894496a3e1aae8859f1ebf6432fb3bd845f356c
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-01-23 09:25:39 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,28 @@
+function solution(array) {
+    let answer = [];
+    let obj= {}
+    
+    array.forEach((n) => {
+    obj[n] = ++obj[n] || 1})
+    
+    for(let key in obj) {
+        answer.push([key, obj[key]])
+    }
+    answer.sort((a, b) => b[1] - a[1])
+
+    if (array.length === 1) return array[0]
+    else if (answer.length > 1 && answer[0][1] === answer[1][1]) return -1
+  
+    return Number(answer[0][0])
+}
+
+
+
+
+
+
+
+
+
+
+
