38a2d14fc69b85a2ed9b0874aa8ab79699536493
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-01-29 21:55:13 +0900

    [Bronze I] Title: 세로읽기, Time: 140 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const maxLength = Math.max(...input.map((i) => i.length))
+let answer = ''
+for (let i = 0; i < maxLength; i++) {
+  for (let j = 0; j < input.length; j++) {
+    answer += input[j][i] || ''
+  }
+}
+console.log(answer)
