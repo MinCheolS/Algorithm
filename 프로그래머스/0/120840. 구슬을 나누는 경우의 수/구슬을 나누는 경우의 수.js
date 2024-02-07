d72cece7bc1fc5eae2e929eb4a376ef51971f882
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-02-08 00:24:14 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -0,0 +1,17 @@
+function solution(balls, share) {
+    let bf= 1
+    let bsf = 1 
+    let sf = 1
+    
+    for(let i = 1; i <= balls; i++) {
+        bf *= i
+    }
+    for(let i = 1; i <= balls - share; i++) {
+        bsf *= i
+    }
+    for(let i = 1; i <= share; i++) {
+        sf *= i
+    }
+    
+    return Math.round(bf / (bsf * sf))
+}
