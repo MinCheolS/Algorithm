b6afc80aa01d63774be75a4b0d4652bd0e6c4ea0
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-02-19 10:48:11 +0900

    [level 0] Title: 합성수 찾기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let result = 0;
+ 
+    function prime(n) {
+        for (let i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i === 0) return true;
+        }
+        return false
+    }
+    
+    for(let i = 0; i <= n; i++){
+        if(prime(i)) result++;
+    }
+    
+    return result;
+}
