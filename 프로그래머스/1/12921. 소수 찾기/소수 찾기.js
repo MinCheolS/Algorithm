377135034ce65c8f9adb15402b7b9eaba74197db
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-02-22 08:13:10 +0900

    [level 1] Title: 소수 찾기, Time: 45.36 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,16 +1,17 @@
 function solution(n) {
-    let numbers = new Set()
+    let answer = 0
+    const arr = new Array(n + 1).fill(1)
     
-    for(let i = 2; i <= n; i++) {
-        numbers.add(i)
-    }
-    
-    for(let j = 2; j < Math.sqrt(n); j++) {
-        if(numbers.has(j)) {
-            for(let k = j * 2; k <= n; k+=j) {
-                numbers.delete(k)
-            }
+    for (let i = 2; i <= n; i++) {
+        if (arr[i] === 0) continue
+        
+        for (let k = i * 2; k <= n; k += i) {
+            arr[k] = 0
         }
     }
-    return numbers.size
+    for (let i = 2; i <= n; i++) {
+        if (arr[i] === 1) answer++
+    }
+    
+    return answer
 }
