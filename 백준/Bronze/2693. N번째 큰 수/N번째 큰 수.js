631bc5521111f4a23022204209a2a3a7e1fe5565
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-02-25 00:10:04 +0900

    [Bronze I] Title: N번째 큰 수, Time: 252 ms, Memory: 11760 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+for (let i = 1; i <= +input[0]; i++) {
+  let arr = input[i].split(' ');
+  arr.sort((a, b) => b - a);
+  console.log(arr[2]);
+}
