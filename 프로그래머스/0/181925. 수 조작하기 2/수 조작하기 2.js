18200f21b896be0104370bc27f30b249791fc32f
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-04-05 08:54:40 +0900

    [level 0] Title: 수 조작하기 2, Time: 7.31 ms, Memory: 41.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,12 @@
+function solution(numLog) {
+    let answer = ''
+    
+    for (let i = 0; i < numLog.length; i++) {
+        if (numLog[i + 1] - numLog[i] === 1) answer += 'w'
+        if (numLog[i + 1] - numLog[i] === -1) answer += 's'
+        if (numLog[i + 1] - numLog[i] === 10) answer += 'd'
+        if (numLog[i + 1] - numLog[i] === -10) answer += 'a'
+    }
+    
+    return answer
+}
