2ec4e4ac5ff0166682a808a103b80e8bffd02792
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-06-01 15:27:22 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -0,0 +1,10 @@
+function solution(arr, flag) {
+    const answer = []
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (flag[i] === true) answer.push(...Array(arr[i] * 2).fill(arr[i]))
+        else answer.splice(answer.length - arr[i], arr[i])
+    }
+    
+    return answer
+}
