31f3ad957f936ff7266eeb67d61402bb9a701121
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-06-06 13:23:59 +0900

    [level 0] Title: 문자열 묶기, Time: 44.85 ms, Memory: 55.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,11 @@
+function solution(strArr) {
+    const answer = []
+    let MaxNum = Math.max(...strArr.map((a) => a.length))
+    
+    for(let i = 1; i <= MaxNum; i++) {
+        const arr = strArr.filter((a) => a.length === i)
+        answer.push(arr.length)
+    }
+
+    return Math.max(...answer)
+}
