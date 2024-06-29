6c6b3d17862051bc4565bbc4529f4fa3630214ac
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-06-29 19:58:07 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.83 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,16 @@
+function solution(seoul) {
+    var answer = '';
+    let a, b
+
+    
+    for(let i = 0; i < seoul.length; i++) {
+       for(let j = 0; j < seoul.length; j++) {    
+           a = seoul[j]
+           }
+       }
+    b = seoul.indexOf("Kim")
+    
+    answer = `김서방은 ${b}에 있다`
+    
+    return answer;
+}
