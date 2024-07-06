c58ba353dc089385664f2b8028b6c022ab5fa70e
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-07-06 23:09:00 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,9 +1,3 @@
 function solution(arr) {
-
-    var result = arr.reduce(function add(sum, currValue) {
-           return sum + currValue
-    }, 0)
-
-    return average = result / arr.length;
-
+    return arr.reduce((acc, cur) => acc + cur) / arr.length
 }
