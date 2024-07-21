902e3aebb85902ba027158f147b5d7fccc317cd8
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-07-21 23:41:54 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    
+    n = n.toString(3)
+    .split("")
+    .reverse()
+    .join('')
+    
+    answer = parseInt(n, 3)
+    
+    return answer;
+}
