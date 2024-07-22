892e3d8a8c6007f49b28286922bf8cdd3cd748fe
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-07-22 10:17:29 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,9 +1,8 @@
 function solution(a, b) {
-    var answer = 1234567890;
     let result = 0
     
     for(let i = 0; i < a.length; i++) {
-            result = a[i]*b[i] + result
+        result = a[i]*b[i] + result
     }
     
     return result;
