723bbc1818587c39ade937548f5bb81f238f9d99
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-07-26 16:10:09 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.18 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,4 @@
-function solution(phone_number) {
-    var answer = " "
-    
+function solution(phone_number) {   
     let a = phone_number.slice(0,phone_number.length-4)
     let replace_a = a.replace(/[0-9]/gi, "*")
     let s = phone_number.slice(-4)
