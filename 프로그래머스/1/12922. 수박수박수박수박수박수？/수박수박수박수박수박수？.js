2d464e9fdc8647ecbfdb8eb0fe149a479306083c
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-07-31 08:26:09 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.33 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,20 +1,21 @@
 function solution(n) {
-    var all = []
+    const all = []
     let a = "수"
     let b = "박"
     let answer
     
     for(let i = 1; i < n+1; i++) {
-        if(i%2 === 1) {
+        if(i % 2 === 1) {
             all.push(a)
         }
-        if(i%2 === 0) {
+        if(i % 2 === 0) {
             all.push(b)
         }        
     }
       
     answer = all.reduce(function(z,x) {
-        return answer = z+x
+        return answer = z + x
     })
+    
     return answer
 }
