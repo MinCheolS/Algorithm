034686920b5c282056c717ea1ed33108da97554b
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-08-05 23:12:46 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,6 +1,3 @@
 function solution(num_list, n) {
-    const firstNum = num_list.slice(n)
-    const secNum = num_list.slice(0, n)
-    
-    return [...firstNum, ...secNum]
+    return [...num_list.slice(n), ...num_list.slice(0, n)]
 }
