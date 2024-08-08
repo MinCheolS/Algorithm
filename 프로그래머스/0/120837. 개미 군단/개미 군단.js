623d3e238061fbd299d6808ce6ec30882af38b6c
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-08-08 22:07:40 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,7 +1,3 @@
 function solution(hp) {
-    let general = Math.floor(hp / 5)
-    let soldier = Math.floor((hp % 5) / 3)
-    let worker = Math.floor((hp % 5 % 3) / 1) 
-
-    return general + soldier + worker
+    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + Math.floor((hp % 5 % 3) / 1) 
 }
