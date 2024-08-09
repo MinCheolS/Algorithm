2668c9a1a3e9bfdffe3d71e3baa01c08b61c333d
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-08-09 10:02:03 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
-    const arrLength = numbers.length
     numbers.sort((a, b) => a - b)
+
     return Math.max(numbers[0] * numbers[1], 
-                    numbers[arrLength - 2] * numbers[arrLength - 1])
+                    numbers[numbers.length - 2] * numbers[numbers.length - 1])
 }
