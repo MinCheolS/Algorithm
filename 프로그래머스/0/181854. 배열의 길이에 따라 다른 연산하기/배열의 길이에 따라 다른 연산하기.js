88e63d4263013c64015dad9990f2d03d3a7f6f83
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-08-11 12:04:22 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -1,8 +1,3 @@
 function solution(arr, n) {
-    if(arr.length % 2 === 0) {
-        return arr.map((a, i) => i % 2 !== 0 ? a + n : a )
-    }
-    else {
-        return arr.map((a, i) => i % 2 === 0 ? a + n : a )
-    }
+    return arr.length % 2 === 0 ? arr.map((a, i) => i % 2 !== 0 ? a + n : a ) : arr.map((a, i) => i % 2 === 0 ? a + n : a )
 }
