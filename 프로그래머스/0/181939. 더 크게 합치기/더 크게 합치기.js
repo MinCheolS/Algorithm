bee40237643dd3cce71c59a12c56df0eac446f97
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-08-13 21:40:56 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,5 +1,3 @@
 function solution(a, b) {
-    let answer = String(a) + b > String(b) + a ? String(a) + b : String(b) + a
- 
-    return Number(answer)
+    return Number(String(a) + b > String(b) + a ? String(a) + b : String(b) + a)
 }
