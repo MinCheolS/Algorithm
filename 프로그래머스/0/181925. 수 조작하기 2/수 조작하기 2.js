323e83af8d58c0466112628ba7772e39231da5e8
신민철 <97032929+MinCheolS@users.noreply.github.com>
2024-08-18 23:18:20 +0900

    [level 0] Title: 수 조작하기 2, Time: 9.84 ms, Memory: 41.4 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -2,10 +2,12 @@ function solution(numLog) {
     let answer = ''
     
     for (let i = 0; i < numLog.length; i++) {
-        if (numLog[i + 1] - numLog[i] === 1) answer += 'w'
-        if (numLog[i + 1] - numLog[i] === -1) answer += 's'
-        if (numLog[i + 1] - numLog[i] === 10) answer += 'd'
-        if (numLog[i + 1] - numLog[i] === -10) answer += 'a'
+        const exp = numLog[i + 1] - numLog[i]
+        
+        if (exp === 1) answer += 'w'
+        if (exp === -1) answer += 's'
+        if (exp === 10) answer += 'd'
+        if (exp === -10) answer += 'a'
     }
     
     return answer
